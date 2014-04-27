b5d2bf98f6b30e3cfe08dea6a9b52f426b713367
Amandeep Singh <amandeeps@outlook.in>
2014-04-27 20:30:09 +0530

    Major: Adding Speech Manager

---
+++ SpeechManager/TextDisplay.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace SpeechManager
+{
+    partial class TextDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTextBox.Location = new System.Drawing.Point(0, 0);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(784, 161);
+            this.mainTextBox.TabIndex = 0;
+            // 
+            // TextDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 161);
+            this.ControlBox = false;
+            this.Controls.Add(this.mainTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TextDisplay";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "TextDisplay";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.TextDisplay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainTextBox;
+    }
+}
---
+++ SpeechManager/TextDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechManager
+{
+    public partial class TextDisplay : Form
+    {
+        public TextDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void TextDisplay_Load(object sender, EventArgs e)
+        {
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int delta = screenWidth - this.Width;
+            this.Location = new Point(delta / 2, 0);
+        }
+    }
+}
